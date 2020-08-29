5d9bca44eccac89284782a866ebffb8f608eda8d
Leandro Felix <felix.neves2@hotmail.com>
2020-08-29 18:43:28 -0300

    1- Implementado método de criar, listar e buscar por ID as categorias. 2- Alterados aplication.prpoperties para não aceitar propriedades desconhecidas.

--- src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -1,11 +1,22 @@
 package com.example.algamoney.api.resource;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.repository.CategoriaRepository;
@@ -15,11 +26,29 @@ import com.example.algamoney.api.repository.CategoriaRepository;
 public class CategoriaResource {
 	
 	@Autowired
-	private CategoriaRepository categorioaRepository;
+	private CategoriaRepository categoriaRepository;
 	
 	@GetMapping
 	public List<Categoria> listar(){
-		return categorioaRepository.findAll();
+		return categoriaRepository.findAll();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+		
+		URI uri = ServletUriComponentsBuilder
+					.fromCurrentRequestUri().path("/{codigo}")
+					.buildAndExpand(categoriaSalva.getCodigo())
+					.toUri();
+		
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<?> buscarPorCodigo(@PathVariable Long codigo) {
+		Optional<Categoria> categoria = categoriaRepository.findById(codigo);
+		return categoria.isPresent() ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build(); 
 	}
 
 }
